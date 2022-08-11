f83e16c4064ecbd06a14d7f0dd12145682b16d65
MMolina <marcosgmolina998@gmail.com>
2022-08-11 19:37:40 -0300

    Entrega fetch

--- carritoCompras.js
+++ carritoCompras.js
@@ -7,25 +7,47 @@ export const carritoCompras = (productoId) =>{
     const containerCarrito = document.getElementById("divCarrito");
 
 
-    const renderizarProductosCarrito = () =>{
+    const renderizarProductosCarrito = async() =>{
 
-        let producto = productosFutbol.find(producto => producto.id ===  productoId);
-        carrito.push(producto);
+        fetch("/stock.json")
+        .then(data => data.json())
+        .then(productos =>{
+            let productoStock = productos.find(productos => productos.id ===  productoId);
+            carrito.push(productoStock);
+    
+            productos.cantidad=1;
+    
+            let div = document.createElement("div");
+    
+            div.classList.add("productosCarrito");
+    
+            div.innerHTML = `<h3>${productoStock.item}</h3>
+                            <h4>Precio:${productoStock.precio}</h4>
+                            <h4>Cantidad:${productoStock.cantidad}</h4>`
+    
+            containerCarrito.appendChild(div);
+            console.log(`<h3>${productoStock.item}</h3>
+            <h4>Precio:${productoStock.precio}</h4>
+            <h4>Cantidad:${productoStock.cantidad}</h4>`);
+        })
 
-        producto.cantidad=1;
+        // let producto = productosFutbol.find(producto => producto.id ===  productoId);
+        // carrito.push(producto);
 
-        let div = document.createElement("div");
+        // producto.cantidad=1;
 
-        div.classList.add("productosCarrito");
+        // let div = document.createElement("div");
 
-        div.innerHTML = `<h3>${producto.item}</h3>
-                        <h4>Precio:${producto.precio}</h4>
-                        <h4>Cantidad:${producto.cantidad}</h4>`
+        // div.classList.add("productosCarrito");
 
-        containerCarrito.appendChild(div);
-        console.log(`<h3>${producto.item}</h3>
-        <h4>Precio:${producto.precio}</h4>
-        <h4>Cantidad:${producto.cantidad}</h4>`);
+        // div.innerHTML = `<h3>${producto.item}</h3>
+        //                 <h4>Precio:${producto.precio}</h4>
+        //                 <h4>Cantidad:${producto.cantidad}</h4>`
+
+        // containerCarrito.appendChild(div);
+        // console.log(`<h3>${producto.item}</h3>
+        // <h4>Precio:${producto.precio}</h4>
+        // <h4>Cantidad:${producto.cantidad}</h4>`);
     }
     renderizarProductosCarrito();
 }
